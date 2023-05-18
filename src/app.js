a3d271c990e4bb6d8e2afefc61ca66332bca15bd
ana_btz <silva.anabtz@gmail.com>
2023-05-18 02:07:38 -0300

    feat: initialize server with express

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
